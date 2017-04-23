88ba0c3664c96bcbd0016bf809d5cce16ce471ca
neopostmodern <clemens@neopostmodern.com>
2017-04-23 15:07:11 +0200

    Add reducers (forgotten in migration)

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,12 +1,16 @@
 // @flow
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
+import { reducer as form } from 'redux-form';
+import apolloClient from '../apollo';
 
 import links from './links';
 import userInterface from './userInterface';
 
 const rootReducer = combineReducers({
   router,
+  form,
+  apollo: apolloClient.reducer(),
   links,
   userInterface,
 });
