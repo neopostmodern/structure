c83cef527eedb23590e6a2ce7f9cd220b32b0b95
neopostmodern <clemens@neopostmodern.com>
2017-06-01 13:20:03 +0200

    Autofocus in AddLink form

--- app/components/AddLink.js
+++ app/components/AddLink.js
@@ -4,22 +4,41 @@ import React, { Component } from 'react';
 import styles from './AddLink.css';
 import { reduxForm, Field } from 'redux-form';
 
-const urlValidator = (value) => (value && value.indexOf('http') === 0 ? undefined : "Did you forget the protocol? (e.g. https://)");
-const renderField = ({ input, placeholder, className, type, meta: { touched, error, warning } }) => (
+const urlValidator = (value) => (value && value.indexOf('http') === 0
+  ? undefined
+  : 'Did you forget the protocol? (e.g. https://)'
+);
+const renderField = ({input, autoFocus, placeholder, className, type, meta: {touched, error, warning}}) => (
   <div>
-    <input {...input} placeholder={placeholder} type={type} className={className} />
-    {touched && ((error && <div style={{marginTop: '0.5em', color: 'darkred'}}>{error}</div>) || (warning && <span>{warning}</span>))}
+    <input
+      {...input}
+      autoFocus={autoFocus}
+      placeholder={placeholder}
+      type={type}
+      className={className}
+    />
+    {touched && (
+      (error &&
+        <div style={{ marginTop: '0.5em', color: 'darkred' }}>
+          {error}
+          </div>
+      ) ||
+      (warning &&
+        <span>{warning}</span>
+      )
+    )}
   </div>
 );
 
 class AddLink extends Component {
   render() {
     return (
-      <div style={{ paddingTop: '20vh' }}>
+      <div style={{paddingTop: '20vh'}}>
         <form onSubmit={this.props.handleSubmit}>
           <Field
             name="url"
             component={renderField}
+            autoFocus
             type="text"
             className={styles.urlInput}
             placeholder="URL here"
@@ -31,4 +50,4 @@ class AddLink extends Component {
   }
 }
 
-export default reduxForm({ form: 'addLink' })(AddLink);
+export default reduxForm({form: 'addLink'})(AddLink)
