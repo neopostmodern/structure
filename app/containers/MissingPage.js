1b77272679bb32cfb8aa4e3603424869aff99f06
neopostmodern <clemens@neopostmodern.com>
2017-06-20 16:52:42 +0200

    Build/deploy fix - include V8 flags on server - formatting fix for webpack/babel

--- app/containers/MissingPage.js
+++ app/containers/MissingPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Centered from '../components/Centered';
 
-export default class MissingPage extends React.Component {
+class MissingPage extends React.Component {
   render() {
-    return <Centered text={"No idea where to find " + this.props.location.pathname} />;
+    return <Centered text={`No idea where to find ${ this.props.location.pathname }`} />;
   }
 }
+
+export default MissingPage;
