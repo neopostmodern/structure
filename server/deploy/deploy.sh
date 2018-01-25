c84a36e5af077ddd8a49eabfa93eb85694dd0288
neopostmodern <clemens@neopostmodern.com>
2018-01-25 02:10:39 +0100

    Configure node args per server

--- server/deploy/deploy.sh
+++ server/deploy/deploy.sh
@@ -13,7 +13,6 @@ ssh "$USER@$SERVER" "cd backend && yarn install --production"
 echo "OK"
 
 echo "Starting backend service (as pm2-process '$PROCESS_NAME')..."
-NODE_ARGS="--node-args=\"--harmony_object_values_entries\""
 ssh "$USER@$SERVER" "pm2 restart $PROCESS_NAME $NODE_ARGS || pm2 start backend/server.js --name $PROCESS_NAME $NODE_ARGS"
 echo "OK"
 
