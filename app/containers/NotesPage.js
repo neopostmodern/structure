1791e09f3ac5eb3c6b5b3c2d9022bebed4256f08
neopostmodern <clemens@neopostmodern.com>
2018-04-06 21:39:03 +0200

    Batch-operations bar sticky to top when scrolling

--- app/containers/NotesPage.js
+++ app/containers/NotesPage.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 import { connect } from 'react-redux';
+import Classnames from 'classnames';
 import Mousetrap from 'mousetrap';
 import makeMousetrapGlobal from '../utils/mousetrapGlobal';
 
@@ -297,8 +298,7 @@ export class NotesPage extends React.Component {
         content.push(
           <div
             key="batch-operations-menu"
-            className={menuStyles.menu}
-            style={{ marginTop: '1rem' }}
+            className={Classnames(menuStyles.menu, menuStyles.sticky)}
           >
             {this.selectedNoteCount()} selected
             (
