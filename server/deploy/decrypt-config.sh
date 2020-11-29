75cd6ff9689eaf746e4cc91695547f80ea3faabe
neopostmodern <clemens@neopostmodern.com>
2020-11-29 17:23:50 +0100

    fix(deploy): decrypt config file into server source folder

--- server/deploy/decrypt-config.sh
+++ server/deploy/decrypt-config.sh
@@ -1,2 +1,1 @@
-gpg --quiet --batch --yes --decrypt --passphrase="${CONFIG_DECRYPT_PASSPHRASE}" --output "${BASH_SOURCE%/*}/config.json" "${BASH_SOURCE%/*}/config.json.gpg"
-gpg --quiet --batch --yes --decrypt --passphrase="${CONFIG_DECRYPT_PASSPHRASE}" --output "${BASH_SOURCE%/*}/config-staging.json" "${BASH_SOURCE%/*}/config-staging.json.gpg"
+gpg --quiet --batch --yes --decrypt --passphrase="${CONFIG_DECRYPT_PASSPHRASE}" --output "${BASH_SOURCE%/*}/../config.json" "${BASH_SOURCE%/*}/${CONFIG_FILE}.gpg"
