f48e765a38fd733360b975ecb86064a77e7a3c90
neopostmodern <clemens@neopostmodern.com>
2020-11-29 18:39:59 +0100

    fix(deploy): keep a copy of config in deploy folder

--- server/deploy/decrypt-config.sh
+++ server/deploy/decrypt-config.sh
@@ -1,1 +1,2 @@
-gpg --quiet --batch --yes --decrypt --passphrase="${CONFIG_DECRYPT_PASSPHRASE}" --output "${BASH_SOURCE%/*}/../lib/config.json" "${BASH_SOURCE%/*}/${CONFIG_FILE}.gpg"
+gpg --quiet --batch --yes --decrypt --passphrase="${CONFIG_DECRYPT_PASSPHRASE}" --output "${BASH_SOURCE%/*}/${CONFIG_FILE}" "${BASH_SOURCE%/*}/${CONFIG_FILE}.gpg"
+cp "${BASH_SOURCE%/*}/${CONFIG_FILE}" "${BASH_SOURCE%/*}/../lib/config.json"
