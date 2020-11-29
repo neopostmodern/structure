3f79cb7904e27577c3ac22922a32dfd4a5f94dbc
neopostmodern <clemens@neopostmodern.com>
2020-11-29 18:16:07 +0100

    fix(deploy): missing folder name in previous commit

--- server/deploy/decrypt-config.sh
+++ server/deploy/decrypt-config.sh
@@ -1,1 +1,1 @@
-gpg --quiet --batch --yes --decrypt --passphrase="${CONFIG_DECRYPT_PASSPHRASE}" --output "${BASH_SOURCE%/*}/../config.json" "${BASH_SOURCE%/*}/${CONFIG_FILE}.gpg"
+gpg --quiet --batch --yes --decrypt --passphrase="${CONFIG_DECRYPT_PASSPHRASE}" --output "${BASH_SOURCE%/*}/../lib/config.json" "${BASH_SOURCE%/*}/${CONFIG_FILE}.gpg"
