d359e9c242504feed60525fd4fab08edd2359457
neopostmodern <clemens@neopostmodern.com>
2020-11-29 18:52:13 +0100

    fix(deploy): ensure there is a copy of the config named "config.json" in the deploy folder

--- server/deploy/decrypt-config.sh
+++ server/deploy/decrypt-config.sh
@@ -1,2 +1,3 @@
 gpg --quiet --batch --yes --decrypt --passphrase="${CONFIG_DECRYPT_PASSPHRASE}" --output "${BASH_SOURCE%/*}/${CONFIG_FILE}" "${BASH_SOURCE%/*}/${CONFIG_FILE}.gpg"
+[[ $CONFIG_FILE != "config.json" ]] && cp "${BASH_SOURCE%/*}/${CONFIG_FILE}" "${BASH_SOURCE%/*}/config.json"
 cp "${BASH_SOURCE%/*}/${CONFIG_FILE}" "${BASH_SOURCE%/*}/../lib/config.json"
