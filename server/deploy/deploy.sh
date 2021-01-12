5255e2d06ebe6152f74d8ceda705f6f936ef20c3
neopostmodern <clemens@neopostmodern.com>
2021-01-12 19:29:23 +0100

    fix: allow specifying the branch to deploy in the config
    - set the staging configuration to actually deploy the staging branch

--- server/deploy/deploy.sh
+++ server/deploy/deploy.sh
@@ -13,7 +13,10 @@ if [ -n "$RUN_PREFLIGHT" ]; then
 fi
 
 echo "Cloning on server..."
-ssh "$USER@$SERVER" "git clone https://github.com/neopostmodern/structure $SERVER_FOLDER_BACKEND"
+if [ -n "$RUN_PREFLIGHT" ]; then
+   GIT_ARGUMENTS="--branch $GIT_BRANCH"
+fi
+ssh "$USER@$SERVER" "git clone $GIT_ARGUMENTS https://github.com/neopostmodern/structure $SERVER_FOLDER_BACKEND"
 echo "OK"
 
 echo "Copy decrypted config file to server..."
