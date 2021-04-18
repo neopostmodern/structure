100aa463698bc0ed6bdb9d43760533bfcf6bbe89
neopostmodern <clemens@neopostmodern.com>
2021-04-18 13:26:40 +0200

    feat: switch to auto-generated workbox serviceworker

--- app/serviceworker/register.js
+++ app/serviceworker/register.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceworker.js').then(
+    navigator.serviceWorker.register('/service-worker.js').then(
       (registration) => {
         // Registration was successful
         console.log(
--- webpack/webpack.config.web.prod.babel.js
+++ webpack/webpack.config.web.prod.babel.js
@@ -26,10 +26,6 @@ export const fileCopyPatterns = [
     from: path.join(__dirname, '../resources/icons/256x256.png'),
     to: 'icons/256x256.png',
   },
-  {
-    from: path.join(__dirname, '../app/serviceworker/serviceworker.js'),
-    to: 'serviceworker.js'
-  },
 ]
 
 const webpackConfig = merge(rendererProdConfig, {
