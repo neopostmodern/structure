ec4d795a571210d38531f9bbde24ee9f4f32510d
neopostmodern <clemens@neopostmodern.com>
2022-03-10 00:05:27 +0000

    feat: minimal styles for rendered markdown

--- client/src/renderer/components/MarkedTextarea.tsx
+++ client/src/renderer/components/MarkedTextarea.tsx
@@ -39,6 +39,40 @@ const TextareaContainer = styled.div`
   min-height: 3rem;
 `;
 
+const RenderedMarkdown = styled.div`
+  @media (prefers-color-scheme: dark) {
+    a {
+      color: #3485ff;
+    }
+  }
+
+  blockquote {
+    margin: 0;
+    border-left: 2px gray solid;
+    padding: 0 1em;
+  }
+
+  code {
+    padding: 0.1em 0.2em;
+    border-radius: 2px;
+    background-color: rgba(128, 128, 128, 0.4);
+  }
+
+  pre {
+    max-width: 100%;
+    overflow-x: auto;
+
+    padding: 0.5em;
+    border-radius: 2px;
+    background-color: rgba(128, 128, 128, 0.4);
+
+    > code {
+      padding: initial;
+      background-color: initial;
+    }
+  }
+`;
+
 const EmptyTextarea = styled.div`
   border: 1px dashed gray;
   color: gray;
@@ -109,7 +143,7 @@ const MarkedTextarea: React.FC<MarkedTextareaProps> = ({ name }) => {
 
     if (textContent && textContent.length) {
       content = (
-        <div
+        <RenderedMarkdown
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{ __html: marked(textContent) }}
           style={{ fontSize: '1rem' }}
