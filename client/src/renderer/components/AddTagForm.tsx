7b1ed7a236cbfef407fbcae16d313f38386488c0
neopostmodern <clemens@neopostmodern.com>
2022-04-06 11:05:47 +0200

    refactor(client): extract AddTagForm
    - add loading indicator

---
+++ client/src/renderer/components/AddTagForm.tsx
@@ -0,0 +1,144 @@
+import { gql, useMutation, useQuery } from '@apollo/client';
+import { AddCircleOutline as PlusIcon } from '@mui/icons-material';
+import { Button, IconButton, Skeleton, Tooltip } from '@mui/material';
+import Mousetrap from 'mousetrap';
+import { useEffect, useState } from 'react';
+import { TAGS_QUERY } from '../containers/TagsPage';
+import {
+  AddTagByNameToNote,
+  AddTagByNameToNoteVariables,
+} from '../generated/AddTagByNameToNote';
+import { TagsQuery, TagsQuery_tags } from '../generated/TagsQuery';
+import makeMousetrapGlobal from '../utils/mousetrapGlobal';
+import InlineTagForm from './InlineTagForm';
+
+export const ADD_TAG_MUTATION = gql`
+  mutation AddTagByNameToNote($noteId: ID!, $tagName: String!) {
+    addTagByNameToNote(noteId: $noteId, name: $tagName) {
+      ... on INote {
+        _id
+        tags {
+          _id
+          name
+          color
+        }
+      }
+    }
+  }
+`;
+
+makeMousetrapGlobal(Mousetrap);
+
+const tagShortcutKeys = ['ctrl+t', 'command+t'];
+
+const AddTagForm = ({
+  noteId,
+  withShortcuts,
+  currentTags,
+}: {
+  noteId: string;
+  withShortcuts?: boolean;
+  currentTags: Array<TagsQuery_tags>;
+}) => {
+  const [addingNew, setAddingNew] = useState<boolean>(false);
+  const [submittedTag, setSubmittedTag] = useState<string | null>(null);
+
+  const tagsQuery = useQuery<TagsQuery>(TAGS_QUERY);
+
+  const [addTagToNote, addTagToNoteMutation] = useMutation<
+    AddTagByNameToNote,
+    AddTagByNameToNoteVariables
+  >(ADD_TAG_MUTATION, {
+    update: (cache, { data: { addTagByNameToNote } }) => {
+      const cacheValue = cache.readQuery<TagsQuery>({ query: TAGS_QUERY });
+
+      if (!cacheValue) {
+        return;
+      }
+      const { tags: tagsInCache } = cacheValue;
+
+      const newTags = addTagByNameToNote.tags.filter(
+        (tag) => !tagsInCache.some(({ _id }) => _id === tag._id)
+      );
+
+      cache.writeQuery({
+        query: TAGS_QUERY,
+        data: { tags: [...tagsInCache, ...newTags] },
+      });
+    },
+  });
+
+  const showNewTagForm = (): void => {
+    setAddingNew(true);
+  };
+
+  const hideNewTagForm = (): void => {
+    setAddingNew(false);
+  };
+
+  useEffect(() => {
+    if (withShortcuts) {
+      Mousetrap.bindGlobal(tagShortcutKeys, showNewTagForm);
+    }
+
+    return (): void => {
+      if (withShortcuts) {
+        Mousetrap.unbind(tagShortcutKeys);
+      }
+    };
+  });
+
+  const handleAddTag = (tagName: string): void => {
+    const tagValue = tagName.trim();
+    if (tagValue.length > 0) {
+      addTagToNote({ variables: { noteId, tagName: tagValue } });
+      setSubmittedTag(tagValue);
+      hideNewTagForm();
+    } else {
+      // eslint-disable-next-line no-undef
+      alert("Can't add empty tag."); // todo: error handling in UI
+    }
+  };
+
+  if (!navigator.onLine) {
+    return null;
+  }
+  if (addTagToNoteMutation.loading) {
+    return (
+      <Skeleton variant="text" height={'2.2rem'}>
+        <div>{submittedTag}</div>
+      </Skeleton>
+    );
+  }
+  if (addingNew) {
+    return (
+      <InlineTagForm
+        tags={
+          tagsQuery.loading
+            ? 'loading'
+            : tagsQuery.data!.tags.filter(
+                (tag) => !currentTags.some(({ _id }) => _id === tag._id)
+              )
+        }
+        onAddTag={handleAddTag}
+        onAbort={hideNewTagForm}
+      />
+    );
+  } else if (currentTags.length === 0) {
+    return (
+      <Button variant="outlined" size="small" onClick={showNewTagForm}>
+        Add tag
+      </Button>
+    );
+  }
+
+  return (
+    <Tooltip title="Add tag">
+      <IconButton size="small" onClick={showNewTagForm}>
+        <PlusIcon />
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export default AddTagForm;
--- client/src/renderer/components/Tags.tsx
+++ client/src/renderer/components/Tags.tsx
@@ -1,40 +1,13 @@
-import { gql, useMutation, useQuery } from '@apollo/client';
-import { AddCircleOutline as PlusIcon } from '@mui/icons-material';
-import { Button, IconButton, Tooltip } from '@mui/material';
-import Mousetrap from 'mousetrap';
-import React, { useEffect, useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
+import React from 'react';
 import styled from 'styled-components';
-import { TAGS_QUERY } from '../containers/TagsPage';
-import {
-  AddTagByNameToNote,
-  AddTagByNameToNoteVariables,
-} from '../generated/AddTagByNameToNote';
 import {
   RemoveTagByIdFromNote,
   RemoveTagByIdFromNoteVariables,
 } from '../generated/RemoveTagByIdFromNote';
-import { TagsQuery } from '../generated/TagsQuery';
-import { TagType } from '../types';
-import makeMousetrapGlobal from '../utils/mousetrapGlobal';
-import InlineTagForm from './InlineTagForm';
+import { TagsQuery_tags } from '../generated/TagsQuery';
+import AddTagForm from './AddTagForm';
 import Tag from './Tag';
-
-makeMousetrapGlobal(Mousetrap);
-
-export const ADD_TAG_MUTATION = gql`
-  mutation AddTagByNameToNote($noteId: ID!, $tagName: String!) {
-    addTagByNameToNote(noteId: $noteId, name: $tagName) {
-      ... on INote {
-        _id
-        tags {
-          _id
-          name
-          color
-        }
-      }
-    }
-  }
-`;
 
 export const REMOVE_TAG_MUTATION = gql`
   mutation RemoveTagByIdFromNote($noteId: ID!, $tagId: ID!) {
@@ -51,8 +24,6 @@ export const REMOVE_TAG_MUTATION = gql`
   }
 `;
 
-const tagShortcutKeys = ['ctrl+t', 'command+t'];
-
 const TagContainer = styled.div`
   display: flex;
   min-width: 0;
@@ -62,7 +33,7 @@ const TagContainer = styled.div`
 `;
 
 interface TagsProps {
-  tags: Array<TagType>;
+  tags: Array<TagsQuery_tags>;
   noteId: string;
   withShortcuts?: boolean;
   size?: 'small' | 'medium';
@@ -74,102 +45,11 @@ const Tags: React.FC<TagsProps> = ({
   size,
   withShortcuts = false,
 }) => {
-  const [addingNew, setAddingNew] = useState<boolean>(false);
-
-  const tagsQuery = useQuery<TagsQuery>(TAGS_QUERY);
-
-  // todo: show loading indicator
-  const [addTagToNote] = useMutation<
-    AddTagByNameToNote,
-    AddTagByNameToNoteVariables
-  >(ADD_TAG_MUTATION, {
-    update: (cache, { data: { addTagByNameToNote } }) => {
-      const cacheValue = cache.readQuery<TagsQuery>({ query: TAGS_QUERY });
-
-      if (!cacheValue) {
-        return;
-      }
-      const { tags: tagsInCache } = cacheValue;
-
-      const newTags = addTagByNameToNote.tags.filter(
-        (tag) => !tagsInCache.some(({ _id }) => _id === tag._id)
-      );
-
-      cache.writeQuery({
-        query: TAGS_QUERY,
-        data: { tags: [...tagsInCache, ...newTags] },
-      });
-    },
-  });
-
   const [removeTagFromNote] = useMutation<
     RemoveTagByIdFromNote,
     RemoveTagByIdFromNoteVariables
   >(REMOVE_TAG_MUTATION);
 
-  const showNewTagForm = (): void => {
-    setAddingNew(true);
-  };
-
-  const hideNewTagForm = (): void => {
-    setAddingNew(false);
-  };
-
-  useEffect(() => {
-    if (withShortcuts) {
-      Mousetrap.bindGlobal(tagShortcutKeys, showNewTagForm);
-    }
-
-    return (): void => {
-      if (withShortcuts) {
-        Mousetrap.unbind(tagShortcutKeys);
-      }
-    };
-  });
-
-  const handleAddTag = (tagName: string): void => {
-    const tagValue = tagName.trim();
-    if (tagValue.length > 0) {
-      addTagToNote({ variables: { noteId, tagName: tagValue } });
-      hideNewTagForm();
-    } else {
-      // eslint-disable-next-line no-undef
-      alert("Can't add empty tag."); // todo: error handling in UI
-    }
-  };
-
-  let newTagForm;
-  if (navigator.onLine) {
-    if (addingNew) {
-      newTagForm = (
-        <InlineTagForm
-          tags={
-            tagsQuery.loading
-              ? 'loading'
-              : tagsQuery.data.tags.filter(
-                  (tag) => !tags.some(({ _id }) => _id === tag._id)
-                )
-          }
-          onAddTag={handleAddTag}
-          onAbort={hideNewTagForm}
-        />
-      );
-    } else if (tags.length === 0) {
-      newTagForm = (
-        <Button variant="outlined" size="small" onClick={showNewTagForm}>
-          Add tag
-        </Button>
-      );
-    } else {
-      newTagForm = (
-        <Tooltip title="Add tag">
-          <IconButton size="small" onClick={showNewTagForm}>
-            <PlusIcon />
-          </IconButton>
-        </Tooltip>
-      );
-    }
-  }
   return (
     <TagContainer>
       {tags.map((tag) => (
@@ -183,7 +63,11 @@ const Tags: React.FC<TagsProps> = ({
           }}
         />
       ))}
-      {newTagForm}
+      <AddTagForm
+        withShortcuts={withShortcuts}
+        noteId={noteId}
+        currentTags={tags}
+      />
     </TagContainer>
   );
 };
