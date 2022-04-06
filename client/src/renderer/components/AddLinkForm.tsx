6cfb86e2410e34d3c371b5379e8ec7c066f0bc9a
neopostmodern <clemens@neopostmodern.com>
2022-04-06 10:17:05 +0200

    feat(client): add submit, paste button to add link form
    - paste button only appears on supported platforms

--- client/src/renderer/components/AddLinkForm.tsx
+++ client/src/renderer/components/AddLinkForm.tsx
@@ -1,18 +1,25 @@
+import { ArrowForward, ContentPaste } from '@mui/icons-material';
+import {
+  FormControl,
+  FormHelperText,
+  IconButton,
+  Input,
+  InputAdornment,
+} from '@mui/material';
 import React, { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { clearClipboard, requestClipboard } from '../actions/userInterface';
 import { RootState } from '../reducers';
-import { TextField } from './CommonStyles';
 
-const AddLinkInput = styled(TextField)`
-  width: 100%;
-  border-bottom: 1px solid gray;
-  font-size: 200%;
+const AddLinkInput = styled(Input)`
+  input {
+    font-size: 200%;
+  }
 `;
 
-const isUrlValid = (url): boolean => url?.startsWith('http');
+const isUrlValid = (url: string): boolean => url.startsWith('http');
 
 type AddLinkFormProps = {
   defaultValue?: string;
@@ -28,7 +35,7 @@ const AddLinkForm: React.FC<AddLinkFormProps> = ({
   onAbort,
 }) => {
   const dispatch = useDispatch();
-  const { register, formState, handleSubmit, setValue } = useForm({
+  const { formState, handleSubmit, setValue, control } = useForm({
     defaultValues: {
       uri: defaultValue || '',
     },
@@ -50,37 +57,78 @@ const AddLinkForm: React.FC<AddLinkFormProps> = ({
     };
   }, [dispatch]);
 
-  const clipboard = useSelector<RootState, string>(
+  const clipboard = useSelector<RootState, string | undefined>(
     (state) => state.userInterface.clipboard
   );
-  if (isUrlValid(clipboard)) {
+  if (clipboard && isUrlValid(clipboard)) {
     setValue('uri', clipboard);
+  }
+
+  let endAdornment: JSX.Element | undefined;
+  if (!Object.keys(formState.errors).length && formState.dirtyFields.uri) {
+    endAdornment = (
+      <IconButton
+        onClick={() => {
+          submitHandler();
+        }}
+      >
+        <ArrowForward />
+      </IconButton>
+    );
+  } else if (navigator.clipboard.readText) {
+    endAdornment = (
+      <IconButton
+        onClick={() => {
+          (async () => {
+            setValue('uri', await navigator.clipboard.readText());
+          })();
+        }}
+      >
+        <ContentPaste />
+      </IconButton>
+    );
   }
 
   return (
     <div style={{ paddingTop: '20vh' }}>
       <form onSubmit={submitHandler}>
-        <AddLinkInput
+        <Controller
           name="uri"
-          autoFocus
-          type="text"
-          placeholder="URL here"
-          {...register('uri', {
+          control={control}
+          rules={{
+            required: 'URL is required',
             validate: (value) =>
               isUrlValid(value) ||
               'Did you forget the protocol? (e.g. https://)',
-          })}
-          onKeyDown={(event): void => {
-            if (event.key === 'Escape') {
-              onAbort();
-            }
           }}
+          render={({ field }) => (
+            <FormControl variant="standard" fullWidth>
+              <AddLinkInput
+                {...field}
+                autoFocus
+                type="text"
+                placeholder="URL here"
+                onKeyDown={(event): void => {
+                  if (event.key === 'Escape') {
+                    onAbort();
+                  }
+                }}
+                endAdornment={
+                  endAdornment ? (
+                    <InputAdornment position="end">
+                      {endAdornment}
+                    </InputAdornment>
+                  ) : undefined
+                }
+              />
+              {formState.errors.uri && (
+                <FormHelperText error>
+                  {formState.errors.uri.message}
+                </FormHelperText>
+              )}
+            </FormControl>
+          )}
         />
-        {formState.errors.uri && (
-          <div style={{ marginTop: '0.5em', color: 'darkred' }}>
-            {formState.errors.uri.message}
-          </div>
-        )}
       </form>
     </div>
   );
