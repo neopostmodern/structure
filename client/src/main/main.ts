neopostmodern/structure

commit 2b5aff2ae66858948404a8e4942a6d9901218c82

build(client): reduce (browser) bundle size
- add babel to enable tree shake
- even better code splitting, makes all JS SW-cached
- remove core-js & regenerator-runtime, see https://github.com/electron-react-boilerplate/electron-react-boilerplate/pull/3132/
- remove now unused mjs build of common

closes #140

client/.babelrc
@@ -1,0 +1,7 @@
+{
+  "presets": [
+    "@babel/env",
+    "@babel/preset-typescript",
+    ["@babel/preset-react", {"runtime": "automatic"}]
+  ]
+}

client/.erb/configs/webpack.config.base.ts
@@ -66,11 +66,7 @@ const configuration: webpack.Configuration = {
         test: /\.[jt]sx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'ts-loader',
-          options: {
-            // Remove this line to enable type checking in webpack builds
-            transpileOnly: true,
-          },
+          loader: 'babel-loader',
         },
       },
     ],

client/.erb/configs/webpack.config.renderer.dev.ts
@@ -53,8 +53,6 @@ const configuration: webpack.Configuration = {
   entry: [
     `webpack-dev-server/client?http://localhost:${port}/dist`,
     'webpack/hot/only-dev-server',
-    'core-js',
-    'regenerator-runtime/runtime',
     path.join(webpackPaths.srcRendererPath, 'index.tsx'),
   ],
 

client/.erb/configs/webpack.config.renderer.prod.ts
@@ -36,13 +36,14 @@ const configuration: webpack.Configuration = {
 
   mode: 'production',
 
-  target: ['web', 'electron-renderer'],
+  target:
+    process.env.TARGET === 'web'
+      ? 'browserslist:defaults'
+      : ['web', 'electron-renderer'],
 
-  entry: [
-    'core-js',
-    'regenerator-runtime/runtime',
-    path.join(webpackPaths.srcRendererPath, 'index.tsx'),
-  ],
+  entry: {
+    main: path.join(webpackPaths.srcRendererPath, 'index.tsx'),
+  },
 
   output: {
     path:
@@ -50,7 +51,8 @@ const configuration: webpack.Configuration = {
         ? webpackPaths.distWebPath
         : webpackPaths.distRendererPath,
     publicPath: process.env.TARGET === 'web' ? '/' : './',
-    filename: process.env.TARGET === 'web' ? '[id].js' : 'renderer.js',
+    filename:
+      process.env.TARGET === 'web' ? '[name].[chunkhash].js' : 'renderer.js',
     library: {
       type: 'umd',
     },
@@ -106,12 +108,21 @@ const configuration: webpack.Configuration = {
       }),
       new CssMinimizerPlugin(),
     ],
-    splitChunks:
-      process.env.TARGET === 'web'
-        ? {
+    ...(process.env.TARGET === 'web'
+      ? {
+          runtimeChunk: true,
+          splitChunks: {
             chunks: 'all',
-          }
-        : false,
+            cacheGroups: {
+              vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name: 'vendors',
+                chunks: 'all',
+              },
+            },
+          },
+        }
+      : {}),
   },
 
   plugins: [

common/scripts/fix-package-type.sh
@@ -1,11 +1,6 @@
-cat >dist/cjs/package.json <<!EOF
+cat >dist/package.json <<!EOF
 {
     "type": "commonjs"
 }
 !EOF
 
-cat >dist/mjs/package.json <<!EOF
-{
-    "type": "module"
-}
-!EOF

commit b53c3ee282fe6a87b1f55d97215de60b5b09dc08

fix: add `@babel/plugin-transform-runtime`
- regression from 2b5aff2ae66858948404a8e4942a6d9901218c82

client/.babelrc
@@ -3,5 +3,6 @@
     "@babel/env",
     "@babel/preset-typescript",
     ["@babel/preset-react", {"runtime": "automatic"}]
-  ]
+  ],
+  "plugins": ["@babel/plugin-transform-runtime"]
 }

client/src/main/main.ts
@@ -9,12 +9,10 @@
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
 import config from '@structure/config';
-import 'core-js/stable';
 import { app, BrowserWindow, ipcMain, shell } from 'electron';
 import log from 'electron-log';
 import { autoUpdater } from 'electron-updater';
 import path from 'path';
-import 'regenerator-runtime/runtime';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 