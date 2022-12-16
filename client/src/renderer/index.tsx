neopostmodern/structure

commit 68b56d0fba1539fd1054761ca3668845b281c6dd

feat: add browser-compatible hotkeys/keyboard shortcuts, remove refresh shortcut

client/src/renderer/index.tsx
@@ -22,6 +22,13 @@ Mousetrap.bind(['esc'], () => {
 Mousetrap.bindGlobal(['ctrl+n', 'command+n'], () => {
   history.push('/notes/add');
 });
+Mousetrap.bindGlobal(['n'], (event: KeyboardEvent) => {
+  // @ts-ignore
+  if (event.target?.nodeName === 'INPUT') {
+    return;
+  }
+  history.push('/notes/add');
+});
 if (process.env.TARGET !== 'web') {
   Mousetrap.bindGlobal(['f12', 'ctrl+shift+i'], () => {
     window.electron.ipcRenderer.toggleDevTools();

client/src/renderer/containers/NotesPage/NotesPage.tsx
@@ -42,8 +42,10 @@ export const NOTES_QUERY = gql`
   ${BASE_NOTE_FRAGMENT}
 `;
 
-const searchFieldShortcutKeys = ['ctrl+f', 'command+f'];
-const reloadShortcutKeys = ['ctrl+r', 'command+r'];
+const searchFieldShortcutKeys =
+  process.env.TARGET === 'web'
+    ? ['ctrl+k', 'command+k']
+    : ['ctrl+f', 'command+f'];
 
 const ShowMore = styled.div`
   color: black;
@@ -90,19 +92,18 @@ const NotesPage: React.FC = () => {
       }
     };
 
-    Mousetrap.bind(searchFieldShortcutKeys, (): void => {
+    Mousetrap.bind(searchFieldShortcutKeys, (event): void => {
+      event.preventDefault();
       searchInput.current?.focus();
       setTimeout(
         () => searchInput.current?.setSelectionRange(0, searchQuery.length),
         10
       );
     });
-    Mousetrap.bind(reloadShortcutKeys, () => notesQuery.refetch());
     window.addEventListener('scroll', handleScrollEvent);
 
     return (): void => {
       Mousetrap.unbind(searchFieldShortcutKeys);
-      Mousetrap.unbind(reloadShortcutKeys);
       window.removeEventListener('scroll', handleScrollEvent);
     };
   }, []);

commit 1380e0728e6ebfe06ee9ec7261382fbcfe948f9c

fix(client): don't treat key presses on textarea as shortcuts
- only applies to non-modifier shortcuts (currently only 'n')
- issue introduced in 68b56d0fba1539fd1054761ca3668845b281c6dd

client/src/renderer/index.tsx
@@ -23,10 +23,15 @@ Mousetrap.bindGlobal(['ctrl+n', 'command+n'], () => {
   history.push('/notes/add');
 });
 Mousetrap.bindGlobal(['n'], (event: KeyboardEvent) => {
-  // @ts-ignore
-  if (event.target?.nodeName === 'INPUT') {
+  if (
+    // @ts-ignore
+    event.target?.nodeName === 'INPUT' ||
+    // @ts-ignore
+    event.target?.nodeName === 'TEXTAREA'
+  ) {
     return;
   }
+  event.preventDefault();
   history.push('/notes/add');
 });
 if (process.env.TARGET !== 'web') {