520c0e2ab069051d6bcc0127ac0ab5d438f35ed0
neopostmodern <neopostmodern@users.noreply.github.com>
2024-09-17 22:19:58 +0200

    feat: increase `entitiesUpdateSince` performance, closes #236 (#237)
    - detailed logging for entitiesUpdatedSince
    - rewrote parts of the fetching procedure to handle pristine fetches more explicitly
    - populate users on pristine entitiesUpdateSince fetch
      by populating the user field deliberately many lookups down the road
    can be saved
    - better leveraging of cached user data

--- server/lib/notes/notesResolvers.ts
+++ server/lib/notes/notesResolvers.ts
@@ -16,12 +16,20 @@ import { logger } from '../util/logging'
 
 const INoteResolvers = {
   async tags(note, args, context) {
+    if (note.tags) {
+      return note.tags.filter(tag => tag.permissions[context.user._id].tag.read)
+    }
+
     return Tag.find({
       ...baseTagsQuery(context.user, 'read'),
       _id: { $in: note.tags },
     }).lean()
   },
   async user(note, args, context) {
+    if (note.user && note.user._id) {
+      return note.user
+    }
+    
     return getCachedUser(note.user, context.user)
   },
 }
