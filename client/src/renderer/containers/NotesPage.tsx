neopostmodern/structure

commit 50d719aa8f71dd0343b1779e9bffd72d67d3e904

feat: improve performance of NotePage and TagPage with fragments and deferred component renders

client/src/renderer/containers/NotesPage.tsx
@@ -1,0 +1,189 @@
+import { gql } from '@apollo/client'
+import { CloudOff } from '@mui/icons-material'
+import { CircularProgress, Stack, Typography } from '@mui/material'
+import React, { useRef } from 'react'
+import { useSelector } from 'react-redux'
+import { LinkLayout } from '../actions/userInterface'
+import Centered from '../components/Centered'
+import FatalApolloError from '../components/FatalApolloError'
+import Gap from '../components/Gap'
+import NetworkOperationsIndicator, {
+  NetworkIndicatorContainer,
+} from '../components/NetworkOperationsIndicator'
+import NoteBatchEditingBar from '../components/NoteBatchEditingBar'
+import NotesList from '../components/NotesList'
+import NotesMenu from '../components/NotesMenu'
+import NotesPageEmpty from '../components/NotesPageEmpty'
+import { SkeletonNoteList } from '../components/Skeletons'
+import useEntitiesUpdatedSince from '../hooks/useEntitiesUpdatedSince'
+import useIsOnline from '../hooks/useIsOnline'
+import useShortcut from '../hooks/useShortcut'
+import useSortedFilteredNotes from '../hooks/useSortedFilteredNotes'
+import { RootState } from '../reducers'
+import { SHORTCUTS } from '../utils/keyboard'
+import noteCountsWithSearchMatchesString from '../utils/noteCountsWithSearchMatchesString'
+import {
+  BASE_NOTE_FRAGMENT,
+  BASE_TAG_FRAGMENT,
+} from '../utils/sharedQueriesAndFragments'
+import { DataState, OFFLINE_CACHE_MISS } from '../utils/useDataState'
+import ComplexLayout from './ComplexLayout'
+
+export const OPTIMISTIC_NOTE_COUNT = 15
+
+export const NOTES_QUERY = gql`
+  query NotesForList {
+    notes {
+      ...BaseNote
+
+      ... on INote {
+        tags {
+          ...BaseTag
+        }
+      }
+    }
+  }
+  ${BASE_NOTE_FRAGMENT}
+  ${BASE_TAG_FRAGMENT}
+`
+
+const NotesPage: React.FC = () => {
+  console.log('notes page')
+  const layout = useSelector<RootState, LinkLayout>(
+    (state) => state.userInterface.linkLayout,
+  )
+  const archiveState = useSelector<RootState, LinkLayout>(
+    (state) => state.userInterface.archiveState,
+  )
+
+  const searchInputRef = useRef<HTMLInputElement | null>(null)
+
+  const cachedFilteredNotesPseudoQuery = useSortedFilteredNotes()
+
+  const entitiesUpdatedSince = useEntitiesUpdatedSince()
+  useShortcut(SHORTCUTS.REFRESH, () => {
+    entitiesUpdatedSince.refetch()
+  })
+
+  useShortcut(SHORTCUTS.SEARCH, () => {
+    searchInputRef.current?.focus()
+    setTimeout(
+      () =>
+        searchInputRef.current?.setSelectionRange(
+          0,
+          searchInputRef.current?.value.length,
+        ),
+      10,
+    )
+  })
+
+  const isOnline = useIsOnline()
+
+  const content = []
+  let primaryActions = null
+
+  if (cachedFilteredNotesPseudoQuery.state === DataState.ERROR) {
+    if (
+      cachedFilteredNotesPseudoQuery.error.extraInfo === OFFLINE_CACHE_MISS &&
+      (entitiesUpdatedSince.state === DataState.UNCALLED ||
+        entitiesUpdatedSince.state === DataState.LOADING)
+    ) {
+      content.push(
+        <Centered key='first-load'>
+          <Stack alignItems='center'>
+            {isOnline ? (
+              <>
+                <CircularProgress color='inherit' disableShrink />
+                <Gap vertical={1} />
+                <Typography variant='caption'>
+                  Loading notes for the first time, this might take a while...
+                </Typography>
+              </>
+            ) : (
+              <>
+                <CloudOff sx={{ fontSize: '4rem', color: 'gray' }} />
+                <Gap vertical={1} />
+                <Typography variant='caption' textAlign='center'>
+                  Your notes are not cached yet and can't be loaded at the
+                  moment because you're offline.
+                  <br /> Please try again later.
+                </Typography>
+              </>
+            )}
+          </Stack>
+        </Centered>,
+      )
+    } else {
+      content.push(
+        <FatalApolloError key='error' query={cachedFilteredNotesPseudoQuery} />,
+      )
+    }
+  } else if (cachedFilteredNotesPseudoQuery.state === DataState.DATA) {
+    if (cachedFilteredNotesPseudoQuery.loadingBackground) {
+      content.push(
+        <NetworkIndicatorContainer key='searching' align='left'>
+          Filtering...
+        </NetworkIndicatorContainer>,
+      )
+    }
+
+    const {
+      allNotes,
+      notes: matchedNotes,
+      archivedCount: archivedMatchedNotesCount,
+    } = cachedFilteredNotesPseudoQuery.data
+
+    content.push(
+      <NoteBatchEditingBar key='batch-operations-menu' notes={matchedNotes} />,
+    )
+
+    content.push(
+      <NetworkOperationsIndicator
+        key='refresh-indicator'
+        query={entitiesUpdatedSince}
+      />,
+    )
+
+    if (matchedNotes.length === 0 && allNotes.length > 0) {
+      content.push(
+        <NotesPageEmpty
+          key='empty-search-filter'
+          archivedMatchedNotesCount={archivedMatchedNotesCount}
+        />,
+      )
+    } else {
+      content.push(
+        <NotesList
+          key='notes-list'
+          noteIds={matchedNotes.map(({ _id }) => _id)}
+          expanded={layout === LinkLayout.EXPANDED_LIST_LAYOUT}
+          initialCount={OPTIMISTIC_NOTE_COUNT}
+        />,
+      )
+    }
+
+    primaryActions = (
+      <NotesMenu
+        noteCountsWithSearchMatchesString={noteCountsWithSearchMatchesString({
+          notes: allNotes,
+          matchedNotes: matchedNotes,
+          archiveState,
+          archivedMatchedNotesCount,
+        })}
+        searchInputRef={searchInputRef}
+      />
+    )
+  }
+
+  return (
+    <ComplexLayout
+      primaryActions={primaryActions}
+      loading={cachedFilteredNotesPseudoQuery.state === DataState.LOADING}
+      loadingComponent={SkeletonNoteList}
+    >
+      {[...content]}
+    </ComplexLayout>
+  )
+}
+
+export default NotesPage

client/src/renderer/containers/NotesPage/NotesPage.tsx
@@ -1,270 +1,0 @@
-import { gql } from '@apollo/client'
-import { useLazyQuery } from '@apollo/client/react'
-import { CloudOff } from '@mui/icons-material'
-import { CircularProgress, Stack, Typography } from '@mui/material'
-import React, { useEffect, useRef, useState } from 'react'
-import { useInView } from 'react-intersection-observer'
-import { useDispatch, useSelector } from 'react-redux'
-import { increaseInfiniteScroll, LinkLayout } from '../../actions/userInterface'
-import Centered from '../../components/Centered'
-import FatalApolloError from '../../components/FatalApolloError'
-import Gap from '../../components/Gap'
-import NetworkOperationsIndicator, {
-  NetworkIndicatorContainer,
-} from '../../components/NetworkOperationsIndicator'
-import NoteBatchEditingBar from '../../components/NoteBatchEditingBar'
-import NotesList from '../../components/NotesList'
-import NotesMenu from '../../components/NotesMenu'
-import NotesPageEmpty from '../../components/NotesPageEmpty'
-import { SkeletonNoteList } from '../../components/Skeletons'
-import type {
-  NotesForListQuery,
-  NotesForListQueryVariables,
-} from '../../generated/graphql'
-import useEntitiesUpdatedSince from '../../hooks/useEntitiesUpdatedSince'
-import useFilteredNotes, {
-  FilteredNotesAndAllNotes,
-} from '../../hooks/useFilteredNotes'
-import useIsOnline from '../../hooks/useIsOnline'
-import useOptimisticCache from '../../hooks/useOptimisticCache'
-import useShortcut from '../../hooks/useShortcut'
-import { RootState } from '../../reducers'
-import { UserInterfaceStateType } from '../../reducers/userInterface'
-import { SHORTCUTS } from '../../utils/keyboard'
-import {
-  BASE_NOTE_FRAGMENT,
-  BASE_TAG_FRAGMENT,
-} from '../../utils/sharedQueriesAndFragments'
-import useDataState, {
-  DataState,
-  OFFLINE_CACHE_MISS,
-} from '../../utils/useDataState'
-import ComplexLayout from '../ComplexLayout'
-
-export const NOTES_QUERY = gql`
-  query NotesForList {
-    notes {
-      ...BaseNote
-
-      ... on INote {
-        tags {
-          ...BaseTag
-        }
-      }
-    }
-  }
-  ${BASE_NOTE_FRAGMENT}
-  ${BASE_TAG_FRAGMENT}
-`
-
-export const OPTIMISTIC_NOTE_COUNT = 10
-
-const NotesPage: React.FC = () => {
-  const { linkLayout: layout, infiniteScrollLimit } = useSelector<
-    RootState,
-    UserInterfaceStateType
-  >((state) => state.userInterface)
-  const [noteRenderLimit, setNoteRenderLimit] = useState<null | number>(
-    OPTIMISTIC_NOTE_COUNT,
-  )
-  const dispatch = useDispatch()
-  const searchInput = useRef<HTMLInputElement | null>(null)
-  const [fetchNotesQuery, notesQuery] = useDataState(
-    useLazyQuery<NotesForListQuery>(NOTES_QUERY, {
-      fetchPolicy: 'cache-only',
-    }),
-  )
-  useEffect(() => {
-    setTimeout(fetchNotesQuery, 100)
-  }, [])
-  const filteredNotesQueryWrapper = useFilteredNotes(notesQuery)
-  const cachedFilteredNotesQueryWrapper = useOptimisticCache<
-    FilteredNotesAndAllNotes,
-    NotesForListQueryVariables,
-    NotesForListQuery
-  >(filteredNotesQueryWrapper, 'notes', ({ notes, archivedCount }) => ({
-    allNotes: null as any, // hack, will not render notes stats from optimistic cache
-    notes: notes.slice(0, OPTIMISTIC_NOTE_COUNT),
-    archivedCount,
-  }))
-
-  const entitiesUpdatedSince = useEntitiesUpdatedSince()
-
-  useShortcut(SHORTCUTS.SEARCH, () => {
-    searchInput.current?.focus()
-    setTimeout(
-      () =>
-        searchInput.current?.setSelectionRange(
-          0,
-          searchInput.current?.value.length,
-        ),
-      10,
-    )
-  })
-
-  const { ref: showMoreElement, inView } = useInView({
-    rootMargin: '0% 0% 20% 0%', // triggers 20vh below viewport
-  })
-
-  // 'rerun' is a dummy variable with no meaning to trigger a re-run of below
-  // hook to check if the element is still in view
-  const [rerun, setRerun] = useState(false)
-  useEffect(() => {
-    if (
-      !inView ||
-      (noteRenderLimit === null &&
-        cachedFilteredNotesQueryWrapper.state === DataState.DATA &&
-        cachedFilteredNotesQueryWrapper.data.notes.length < infiniteScrollLimit)
-    ) {
-      return
-    }
-    if (noteRenderLimit && noteRenderLimit < infiniteScrollLimit) {
-      setNoteRenderLimit(null)
-    } else {
-      dispatch(increaseInfiniteScroll(10))
-    }
-
-    const rerunTimeout = setTimeout(() => {
-      setRerun(!rerun)
-    }, 100)
-
-    return (): void => {
-      clearTimeout(rerunTimeout)
-    }
-  }, [inView, rerun, setRerun])
-
-  useShortcut(SHORTCUTS.REFRESH, () => {
-    entitiesUpdatedSince.refetch()
-  })
-
-  const isOnline = useIsOnline()
-
-  const content = []
-  let primaryActions = null
-
-  if (cachedFilteredNotesQueryWrapper.state === DataState.ERROR) {
-    if (
-      cachedFilteredNotesQueryWrapper.error.extraInfo === OFFLINE_CACHE_MISS &&
-      (entitiesUpdatedSince.state === DataState.UNCALLED ||
-        entitiesUpdatedSince.state === DataState.LOADING)
-    ) {
-      content.push(
-        <Centered key='first-load'>
-          <Stack alignItems='center'>
-            {isOnline ? (
-              <>
-                <CircularProgress color='inherit' disableShrink />
-                <Gap vertical={1} />
-                <Typography variant='caption'>
-                  Loading notes for the first time, this might take a while...
-                </Typography>
-              </>
-            ) : (
-              <>
-                <CloudOff sx={{ fontSize: '4rem', color: 'gray' }} />
-                <Gap vertical={1} />
-                <Typography variant='caption' textAlign='center'>
-                  Your notes are not cached yet and can't be loaded at the
-                  moment because you're offline.
-                  <br /> Please try again later.
-                </Typography>
-              </>
-            )}
-          </Stack>
-        </Centered>,
-      )
-    } else {
-      content.push(
-        <FatalApolloError
-          key='error'
-          query={cachedFilteredNotesQueryWrapper}
-        />,
-      )
-    }
-  } else if (cachedFilteredNotesQueryWrapper.state === DataState.DATA) {
-    if (cachedFilteredNotesQueryWrapper.loadingBackground) {
-      content.push(
-        <NetworkIndicatorContainer key='searching' align='left'>
-          Filtering...
-        </NetworkIndicatorContainer>,
-      )
-    }
-
-    const {
-      allNotes,
-      notes: matchedNotes,
-      archivedCount: archivedMatchedNotesCount,
-    } = cachedFilteredNotesQueryWrapper.data
-
-    content.push(
-      <NoteBatchEditingBar key='batch-operations-menu' notes={matchedNotes} />,
-    )
-
-    content.push(
-      <NetworkOperationsIndicator
-        key='refresh-indicator'
-        query={entitiesUpdatedSince}
-      />,
-    )
-
-    if (
-      matchedNotes.length === 0 &&
-      notesQuery.state === DataState.DATA &&
-      notesQuery.data.notes.length > 0
-    ) {
-      content.push(
-        <NotesPageEmpty
-          key='empty-search-filter'
-          archivedMatchedNotesCount={archivedMatchedNotesCount}
-        />,
-      )
-    } else {
-      content.push(
-        <NotesList
-          key='notes-list'
-          notes={matchedNotes.slice(0, noteRenderLimit || infiniteScrollLimit)}
-          expanded={layout === LinkLayout.EXPANDED_LIST_LAYOUT}
-        />,
-        <div key='more' ref={showMoreElement}>
-          {matchedNotes.length > infiniteScrollLimit &&
-            noteRenderLimit === null && (
-              <>
-                <SkeletonNoteList />
-                <Gap vertical={2} />
-                <Typography
-                  variant='caption'
-                  color='gray'
-                  textAlign='center'
-                  component='div'
-                >
-                  {matchedNotes.length - infiniteScrollLimit} more notes
-                  loading...
-                </Typography>
-              </>
-            )}
-        </div>,
-      )
-    }
-
-    primaryActions = (
-      <NotesMenu
-        matchedNotes={matchedNotes}
-        notes={allNotes}
-        searchInput={searchInput}
-        archivedMatchedNotesCount={archivedMatchedNotesCount}
-      />
-    )
-  }
-
-  return (
-    <ComplexLayout
-      primaryActions={primaryActions}
-      loading={cachedFilteredNotesQueryWrapper.state === DataState.LOADING}
-      loadingComponent={SkeletonNoteList}
-    >
-      {[...content]}
-    </ComplexLayout>
-  )
-}
-
-export default NotesPage

client/src/renderer/components/NoteInList.style.ts
@@ -1,6 +1,6 @@
 import { Button, keyframes } from '@mui/material'
 import styled from 'styled-components'
-import { OPTIMISTIC_NOTE_COUNT } from '../containers/NotesPage/NotesPage'
+import { OPTIMISTIC_NOTE_COUNT } from '../containers/NotesPage'
 import { ExternalLink, InternalLink } from './CommonStyles'
 import UserAvatar from './UserAvatar'
 

client/src/renderer/components/NoteInList.tsx
@@ -1,8 +1,14 @@
+import { gql } from '@apollo/client'
+import { useFragment } from '@apollo/client/react'
 import React, { lazy } from 'react'
 import TimeAgo from 'react-timeago'
-import type { NotesForListQuery } from '../generated/graphql'
+import { NoteInListFragment } from '../generated/graphql'
 import useUserId from '../hooks/useUserId'
 import { noteUrl } from '../utils/routes'
+import {
+  BASE_TAG_FRAGMENT,
+  BASE_USER_FRAGMENT,
+} from '../utils/sharedQueriesAndFragments'
 import suspenseWrap from '../utils/suspenseWrap'
 import * as Styled from './NoteInList.style'
 import NoteInListBatchEditing from './NoteInListBatchEditing'
@@ -15,11 +21,55 @@ const RenderedMarkdown = suspenseWrap(
 )
 
 export const NoteInList: React.FC<{
-  note: NotesForListQuery['notes'][number]
+  noteId: string
   expanded: boolean
   shortcut?: Array<string>
-}> = ({ note, expanded, shortcut }) => {
+}> = ({ noteId, expanded, shortcut }) => {
   const userId = useUserId()
+
+  const noteData = useFragment<NoteInListFragment>({
+    fragment: gql`
+      fragment NoteInList on INote {
+        ... on INote {
+          # type
+          _id
+          name
+          createdAt
+          updatedAt
+          changedAt
+          archivedAt
+          deletedAt
+          description
+          tags {
+            ...BaseTag
+          }
+          user {
+            ...BaseUser
+          }
+        }
+        ... on Link {
+          url
+          domain
+        }
+      }
+      ${BASE_USER_FRAGMENT}
+      ${BASE_TAG_FRAGMENT}
+    `,
+    fragmentName: 'NoteInList',
+    from: {
+      __typename: 'Text',
+      _id: noteId,
+    },
+  })
+  if (noteData.dataState === 'partial') {
+    console.error(
+      `[NoteInList] Partial data received from cache. Missing data: ${noteData.missing}`,
+    )
+    throw Error('[NoteInList] Received partial data.')
+  }
+
+  const note = noteData.data
+
   return (
     <Styled.Note archived={Boolean(note.archivedAt)}>
       <NoteInListBatchEditing noteId={note._id} />

client/src/renderer/components/NotesList.tsx
@@ -1,8 +1,9 @@
+import { gql } from '@apollo/client'
+import { useApolloClient } from '@apollo/client/react'
 import { AddCircle, Create } from '@mui/icons-material'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { push } from 'redux-first-history'
-import type { NotesForListQuery } from '../generated/graphql'
 import useQuickNumberShortcuts from '../hooks/useQuickNumberShortcuts'
 import { QUICK_ACCESS_SHORTCUT_PREFIX, SHORTCUTS } from '../utils/keyboard'
 import { noteUrl } from '../utils/routes'
@@ -10,17 +11,57 @@ import EmptyPageInfo from './EmptyPageInfo'
 import Gap from './Gap'
 import NoteInList from './NoteInList'
 import Shortcut from './Shortcut'
+import ShowMore from './ShowMore'
 
 const NotesList: React.FC<{
-  notes: NotesForListQuery['notes']
+  noteIds: Array<string>
   expanded: boolean
-}> = ({ notes, expanded }) => {
+  initialCount: number
+}> = ({ noteIds, expanded, initialCount }) => {
   const dispatch = useDispatch()
-  useQuickNumberShortcuts(notes, (note) => {
+  const apolloClient = useApolloClient()
+
+  useQuickNumberShortcuts(noteIds, (noteId) => {
+    // todo: simplifiy once link and text have been merged into note
+    const link = apolloClient.readQuery({
+      query: gql`
+        query ReadNoteTypeIfLink($noteId: ID!) {
+          link(linkId: $noteId) {
+            __typename
+            _id
+          }
+        }
+      `,
+      variables: {
+        noteId,
+      },
+    })
+    const text = apolloClient.readQuery({
+      query: gql`
+        query ReadNoteTypeIfLink($noteId: ID!) {
+          text(textId: $noteId) {
+            __typename
+            _id
+          }
+        }
+      `,
+      variables: {
+        noteId,
+      },
+    })
+
+    const note = link !== null ? link.link : text.text
+
     dispatch(push(noteUrl(note)))
   })
 
-  if (notes.length === 0) {
+  const [notesDisplayLimit, setNotesDisplayLimit] = useState(initialCount)
+  const handleIsInView = useCallback(
+    () => setNotesDisplayLimit((limit) => limit + 10),
+    [setNotesDisplayLimit],
+  )
+
+  if (noteIds.length === 0) {
     return (
       <EmptyPageInfo
         icon={Create}
@@ -45,10 +86,10 @@ const NotesList: React.FC<{
     <>
       <Gap vertical={1} />
       <div>
-        {notes.map((note, noteIndex) => (
+        {noteIds.slice(0, notesDisplayLimit).map((noteId, noteIndex) => (
           <NoteInList
-            key={note._id}
-            note={note}
+            key={noteId}
+            noteId={noteId}
             expanded={expanded}
             shortcut={
               SHORTCUTS[`${QUICK_ACCESS_SHORTCUT_PREFIX}${noteIndex + 1}`]
@@ -56,6 +97,12 @@ const NotesList: React.FC<{
           />
         ))}
       </div>
+      {noteIds.length > notesDisplayLimit && (
+        <ShowMore
+          onIsInView={handleIsInView}
+          remainingCount={noteIds.length - notesDisplayLimit}
+        />
+      )}
     </>
   )
 }

client/src/renderer/components/NotesMenu.tsx
@@ -1,39 +1,20 @@
-import {
-  BackspaceOutlined as ClearIcon,
-  FilterAlt,
-  List,
-  Search as SearchIcon,
-  Sort,
-  ViewList,
-} from '@mui/icons-material'
-import {
-  FormControl,
-  FormHelperText,
-  IconButton,
-  Input,
-  InputAdornment,
-  InputLabel,
-} from '@mui/material'
+import { FilterAlt, List, Sort, ViewList } from '@mui/icons-material'
 import { Ref, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {
   ArchiveState,
   changeArchiveState,
   changeLinkLayout,
-  changeSearchQuery,
   changeSortBy,
   LinkLayout,
   SortBy,
 } from '../actions/userInterface'
-import NoteCount from '../containers/NotesPage/NoteCount'
-import type { NotesForListQuery } from '../generated/graphql'
 import { RootState } from '../reducers'
 import {
   DEFAULT_ARCHIVE_STATE,
   DEFAULT_SORT_BY,
   UserInterfaceStateType,
 } from '../reducers/userInterface'
-import { SHORTCUTS } from '../utils/keyboard'
 import {
   archiveStateToName,
   layoutToName,
@@ -41,42 +22,26 @@ import {
 } from '../utils/textHelpers'
 import { Menu, MenuSearchFieldContainer } from './Menu'
 import NotesMenuButton from './NotesMenuButton'
-import TooltipWithShortcut from './TooltipWithShortcut'
+import { SearchField } from './SearchField'
 
 interface NotesMenuProps {
-  notes: NotesForListQuery['notes']
-  matchedNotes: NotesForListQuery['notes']
-  archivedMatchedNotesCount: number | undefined
-  searchInput: Ref<HTMLInputElement>
+  noteCountsWithSearchMatchesString: string | null
+  searchInputRef: Ref<HTMLInputElement>
 }
 
 const NotesMenu = ({
-  notes,
-  matchedNotes,
-  archivedMatchedNotesCount,
-  searchInput,
+  searchInputRef,
+  noteCountsWithSearchMatchesString,
 }: NotesMenuProps) => {
   const {
     linkLayout: layout,
     archiveState,
     sortBy,
-    searchQuery,
   } = useSelector<RootState, UserInterfaceStateType>(
     (state) => state.userInterface,
   )
 
   const dispatch = useDispatch()
-
-  const onChangeSearchQuery = useCallback(
-    (value: string): void => {
-      dispatch(changeSearchQuery(value))
-    },
-    [dispatch],
-  )
-  const handleClearSearchText = useCallback(
-    (): void => onChangeSearchQuery(''),
-    [onChangeSearchQuery],
-  )
 
   const handleChangeLayout = useCallback(
     (newLayout: LinkLayout): void => {
@@ -125,46 +90,10 @@ const NotesMenu = ({
         defaultValue={DEFAULT_SORT_BY}
       />
       <MenuSearchFieldContainer>
-        <TooltipWithShortcut
-          title=''
-          shortcut={SHORTCUTS.SEARCH}
-          adjustVerticalDistance={-30}
-          disableFocusListener
-        >
-          <FormControl variant='standard' sx={{ width: '100%' }}>
-            <InputLabel>Search</InputLabel>
-            <Input
-              onChange={({ target: { value } }): void => {
-                onChangeSearchQuery(value)
-              }}
-              value={searchQuery}
-              inputRef={searchInput}
-              endAdornment={
-                <InputAdornment position='end'>
-                  {searchQuery.length > 0 ? (
-                    <IconButton
-                      aria-label='clear text search filter'
-                      onClick={handleClearSearchText}
-                      edge='end'
-                    >
-                      <ClearIcon />
-                    </IconButton>
-                  ) : (
-                    <SearchIcon />
-                  )}
-                </InputAdornment>
-              }
-            />
-            <FormHelperText sx={{ textAlign: 'right' }}>
-              <NoteCount
-                notes={notes}
-                matchedNotes={matchedNotes}
-                archiveState={archiveState}
-                archivedMatchedNotesCount={archivedMatchedNotesCount}
-              />
-            </FormHelperText>
-          </FormControl>
-        </TooltipWithShortcut>
+        <SearchField
+          inputRef={searchInputRef}
+          noteCountsWithSearchMatchesString={noteCountsWithSearchMatchesString}
+        />
       </MenuSearchFieldContainer>
     </Menu>
   )

commit 7152a146d50d2be64c2c8609a58b78505dd53708

fix: correctly show initial loading state on notes page
- regression from 50d719aa8f71dd0343b1779e9bffd72d67d3e904

client/src/renderer/containers/NotesPage.tsx
@@ -3,7 +3,7 @@ import { CloudOff } from '@mui/icons-material'
 import { CircularProgress, Stack, Typography } from '@mui/material'
 import React, { useRef } from 'react'
 import { useSelector } from 'react-redux'
-import { LinkLayout } from '../actions/userInterface'
+import { ArchiveState, LinkLayout } from '../actions/userInterface'
 import Centered from '../components/Centered'
 import FatalApolloError from '../components/FatalApolloError'
 import Gap from '../components/Gap'
@@ -26,7 +26,7 @@ import {
   BASE_NOTE_FRAGMENT,
   BASE_TAG_FRAGMENT,
 } from '../utils/sharedQueriesAndFragments'
-import { DataState, OFFLINE_CACHE_MISS } from '../utils/useDataState'
+import { DataState } from '../utils/useDataState'
 import ComplexLayout from './ComplexLayout'
 
 export const OPTIMISTIC_NOTE_COUNT = 15
@@ -48,11 +48,10 @@ export const NOTES_QUERY = gql`
 `
 
 const NotesPage: React.FC = () => {
-  console.log('notes page')
   const layout = useSelector<RootState, LinkLayout>(
     (state) => state.userInterface.linkLayout,
   )
-  const archiveState = useSelector<RootState, LinkLayout>(
+  const archiveState = useSelector<RootState, ArchiveState>(
     (state) => state.userInterface.archiveState,
   )
 
@@ -83,41 +82,35 @@ const NotesPage: React.FC = () => {
   let primaryActions = null
 
   if (cachedFilteredNotesPseudoQuery.state === DataState.ERROR) {
-    if (
-      cachedFilteredNotesPseudoQuery.error.extraInfo === OFFLINE_CACHE_MISS &&
-      (entitiesUpdatedSince.state === DataState.UNCALLED ||
-        entitiesUpdatedSince.state === DataState.LOADING)
-    ) {
-      content.push(
-        <Centered key='first-load'>
-          <Stack alignItems='center'>
-            {isOnline ? (
-              <>
-                <CircularProgress color='inherit' disableShrink />
-                <Gap vertical={1} />
-                <Typography variant='caption'>
-                  Loading notes for the first time, this might take a while...
-                </Typography>
-              </>
-            ) : (
-              <>
-                <CloudOff sx={{ fontSize: '4rem', color: 'gray' }} />
-                <Gap vertical={1} />
-                <Typography variant='caption' textAlign='center'>
-                  Your notes are not cached yet and can't be loaded at the
-                  moment because you're offline.
-                  <br /> Please try again later.
-                </Typography>
-              </>
-            )}
-          </Stack>
-        </Centered>,
-      )
-    } else {
-      content.push(
-        <FatalApolloError key='error' query={cachedFilteredNotesPseudoQuery} />,
-      )
-    }
+    content.push(
+      <FatalApolloError key='error' query={cachedFilteredNotesPseudoQuery} />,
+    )
+  } else if (cachedFilteredNotesPseudoQuery.state === DataState.LOADING) {
+    content.push(
+      <Centered key='first-load'>
+        <Stack alignItems='center'>
+          {isOnline ? (
+            <>
+              <CircularProgress color='inherit' disableShrink />
+              <Gap vertical={1} />
+              <Typography variant='caption'>
+                Loading notes for the first time, this might take a while...
+              </Typography>
+            </>
+          ) : (
+            <>
+              <CloudOff sx={{ fontSize: '4rem', color: 'gray' }} />
+              <Gap vertical={1} />
+              <Typography variant='caption' textAlign='center'>
+                Your notes are not cached yet and can't be loaded at the moment
+                because you're offline.
+                <br /> Please try again later.
+              </Typography>
+            </>
+          )}
+        </Stack>
+      </Centered>,
+    )
   } else if (cachedFilteredNotesPseudoQuery.state === DataState.DATA) {
     if (cachedFilteredNotesPseudoQuery.loadingBackground) {
       content.push(